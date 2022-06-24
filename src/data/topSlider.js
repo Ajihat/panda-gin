4b46f05c47226fa829943477e1ae9cb19f393e28
Dawid Lechowski <david.lechowski@gmail.com>
2022-06-24 17:31:27 +0200

    Added favicon, connected project with SASS, created data for top-slider

---
+++ src/data/topSlider.js
@@ -0,0 +1,17 @@
+const topSliderData = [
+    {
+        id: 1,
+        header: "Free shipping from 90 euros",
+        text: "Only in Belgium"
+    },
+    {
+        id: 2,
+        header: "From 140 Euros, choose your gift",
+        text: "Avialable now"
+    },
+    {
+        id: 3,
+        header: "Summer sale is now",
+        text: "Discount on more items"
+    }
+]

85b5023a0b36a784ae9ad7313cd12d4ff0ffc72e
Dawid Lechowski <david.lechowski@gmail.com>
2022-12-07 13:39:49 +0100

    add cocktails database

---
+++ frontend/src/data/cocktailsSubmenu/cocktailsSubmenu.ts
@@ -0,0 +1,17 @@
+export const cocktailsSubmenu = [
+	{
+		id: 1,
+		title: 'all',
+		href: 'header',
+	},
+	{
+		id: 2,
+		title: 'classic',
+		href: 'header',
+	},
+	{
+		id: 3,
+		title: 'signature',
+		href: 'header',
+	},
+];
--- frontend/src/pages/Cocktails/Cocktails.tsx
+++ frontend/src/pages/Cocktails/Cocktails.tsx
@@ -1,6 +1,9 @@
 import { Helmet } from 'react-helmet-async';
 
 import { Header } from 'components/Header/Header';
+import { Submenu } from 'components/Submenu/Submenu';
+
+import { cocktailsSubmenu } from 'data/cocktailsSubmenu/cocktailsSubmenu';
 
 import './Cocktails.sass';
 
@@ -12,6 +15,7 @@ export const Cocktails = () => {
 				<meta name='description' content='Discover the complete collection of Panda Gin based cocktails' />
 			</Helmet>
 			<Header smallTitle='Need a cocktail recipe?' bigTitle='Panda Gin Cocktails' />
+			<Submenu data={cocktailsSubmenu} />
 		</div>
 	);
 };
